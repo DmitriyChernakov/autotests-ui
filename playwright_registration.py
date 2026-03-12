edec284113b02ec9090d3d04a5443976cb1c3e02
Dmitriy <ChernakovDMT@yandex.ru>
2026-03-12 23:01:50 +0700

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,31 +1,42 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
 
 with sync_playwright() as playwright:
-    # Открываем браузер и создаем новую страницу
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
-    # Переходим на страницу регистрации
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-    # Заполняем поле Email
+    page.wait_for_timeout(1000)
+
     registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     registration_email_input.fill('user.name@gmail.com')
 
-    # Заполняем поле Username
+    page.wait_for_timeout(1000)
+
     registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
     registration_username_input.fill('username')
 
-    # Заполняем поле Password
+    page.wait_for_timeout(1000)
+
     registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
     registration_password_input.fill('password')
 
-    # Нажимаем на кнопку Registration
+    page.wait_for_timeout(1000)
+
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    # После редиректа проверяем, что заголовок Dashboard отображается
-    dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_header).to_be_visible()
-    expect(dashboard_header).to_have_text('Dashboard')
+    page.wait_for_timeout(1000)
+
+    context.storage_state(path="browser-state.json")
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+
+    page.wait_for_timeout(5000)
