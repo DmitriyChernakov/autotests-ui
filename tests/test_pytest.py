7418048c609908e94f525c43300d491610397482
Dmitriy <ChernakovDMT@yandex.ru>
2026-03-13 14:27:18 +0700

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,18 @@
+def test_first_try():
+    print("Hello World!")
+
+
+class TestUserLogin:
+    def test_1(self):
+        ...
+
+    def test_2(self):
+        ...
+
+
+def test_assert_positive_case():
+    assert 2 + 2 == 4
+
+
+def test_assert_negative_case():
+    assert 2 + 2 == 5, "2 + 2 != 5"
