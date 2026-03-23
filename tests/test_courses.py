aee604be624fdd37859fe49d6fffc6e272376a67
Dmitriy <ChernakovDMT@yandex.ru>
2026-03-23 17:13:06 +0700

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -7,7 +7,7 @@ from pages.create_course_page import CreateCoursePage
 @pytest.mark.courses
 @pytest.mark.regression
 def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
-    create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
 
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disable_create_course_button()
@@ -15,41 +15,33 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     create_course_page.check_visible_image_upload_view()
 
     create_course_page.check_visible_create_course_form(
-        title='', estimated_time='', description='', max_score='0', min_score='0'
+        title="", estimated_time="", description="", max_score="0", min_score="0"
     )
 
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
 
-    create_course_page.upload_preview_image('./testdata/files/image.png')
+    create_course_page.upload_preview_image("./testdata/files/image.png")
     create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
 
     create_course_page.fill_create_course_form(
-        title='Playwright',
-        estimated_time='2 weeks',
-        description='Playwright',
-        max_score='100',
-        min_score='10',
+        title="Playwright", estimated_time="2 weeks", description="Playwright", max_score="100", min_score="10"
     )
     create_course_page.click_create_course_button()
 
     courses_list_page.check_visible_courses_title()
     courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(
-        index=0,
-        title='Playwright',
-        max_score='100',
-        min_score='10',
-        estimated_time='2 weeks',
+    courses_list_page.course_view.check_visible(
+        index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
     )
 
 
 @pytest.mark.courses
 @pytest.mark.regression
 def test_empty_courses_list(courses_list_page: CoursesListPage):
-    courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-    courses_list_page.navbar.check_visible('username')
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.navbar.check_visible("username")
     courses_list_page.sidebar.check_visible()
     courses_list_page.check_visible_courses_title()
     courses_list_page.check_visible_create_course_button()
