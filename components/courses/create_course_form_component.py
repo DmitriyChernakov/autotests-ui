3bd301450c721e93e0cfc487bf021ffcbdfe056b
Dmitriy <ChernakovDMT@yandex.ru>
2026-03-31 14:23:21 +0700

    Практическое применение PageFactory в автотестах. Практикуемся в применении Page Factory

--- components/courses/create_course_form_component.py
+++ components/courses/create_course_form_component.py
@@ -1,48 +1,56 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
+from elements.input import Input
+from elements.textarea import Textarea
 
 
 class CreateCourseFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.title_input = page.get_by_test_id('create-course-form-title-input').locator('input')
-        self.estimated_time_input = page.get_by_test_id('create-course-form-estimated-time-input').locator('input')
-        self.description_textarea = (
-            page.get_by_test_id('create-course-form-description-input').locator('textarea').first
+        self.title_input = Input(page=page, locator='create-course-form-title-input', name='Title')
+        self.estimated_time_input = Input(
+            page=page,
+            locator='create-course-form-estimated-time-input',
+            name='Estimated time',
         )
-        self.max_score_input = page.get_by_test_id('create-course-form-max-score-input').locator('input')
-        self.min_score_input = page.get_by_test_id('create-course-form-min-score-input').locator('input')
+        self.description_textarea = Textarea(
+            page=page,
+            locator='create-course-form-description-input',
+            name='Description',
+        )
+        self.max_score_input = Input(page=page, locator='create-course-form-max-score-input', name='Max score')
+        self.min_score_input = Input(page=page, locator='create-course-form-min-score-input', name='Min score')
 
     def fill(self, title: str, estimated_time: str, description: str, max_score: str, min_score: str):
-        self.title_input.fill(title)
-        expect(self.title_input).to_have_value(title)
+        self.title_input.fill(value=title)
+        self.title_input.check_have_value(value=title)
 
-        self.estimated_time_input.fill(estimated_time)
-        expect(self.estimated_time_input).to_have_value(estimated_time)
+        self.estimated_time_input.fill(value=estimated_time)
+        self.estimated_time_input.check_have_value(value=estimated_time)
 
-        self.description_textarea.fill(description)
-        expect(self.description_textarea).to_have_value(description)
+        self.description_textarea.fill(value=description)
+        self.description_textarea.check_have_value(value=description)
 
-        self.max_score_input.fill(max_score)
-        expect(self.max_score_input).to_have_value(max_score)
+        self.max_score_input.fill(value=max_score)
+        self.max_score_input.check_have_value(value=max_score)
 
-        self.min_score_input.fill(min_score)
-        expect(self.min_score_input).to_have_value(min_score)
+        self.min_score_input.fill(value=min_score)
+        self.min_score_input.check_have_value(value=min_score)
 
     def check_visible(self, title: str, estimated_time: str, description: str, max_score: str, min_score: str):
-        expect(self.title_input).to_be_visible()
-        expect(self.title_input).to_have_value(title)
+        self.title_input.check_visible()
+        self.title_input.check_have_value(value=title)
 
-        expect(self.estimated_time_input).to_be_visible()
-        expect(self.estimated_time_input).to_have_value(estimated_time)
+        self.estimated_time_input.check_visible()
+        self.estimated_time_input.check_have_value(value=estimated_time)
 
-        expect(self.description_textarea).to_be_visible()
-        expect(self.description_textarea).to_have_value(description)
+        self.description_textarea.check_visible()
+        self.description_textarea.check_have_value(value=description)
 
-        expect(self.max_score_input).to_be_visible()
-        expect(self.max_score_input).to_have_value(max_score)
+        self.max_score_input.check_visible()
+        self.max_score_input.check_have_value(value=max_score)
 
-        expect(self.min_score_input).to_be_visible()
-        expect(self.min_score_input).to_have_value(min_score)
+        self.min_score_input.check_visible()
+        self.min_score_input.check_have_value(value=min_score)
