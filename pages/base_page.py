7cdcac3553e732c4becbff3d7efbbb6abb2a1309
Dmitriy <ChernakovDMT@yandex.ru>
2026-04-01 20:40:56 +0700

    Наращивание тестовой базы и рефакторинг

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
-from playwright.sync_api import Page
-from urllib3.util import wait_for_write
+from typing import Pattern
+
+from playwright.sync_api import Page, expect
 
 
 class BasePage:
@@ -11,3 +12,6 @@ class BasePage:
 
     def reload(self):
         self.page.reload(wait_until='domcontentloaded')
+
+    def check_current_url(self, expected_url: Pattern[str]):
+        expect(self.page).to_have_url(expected_url)
