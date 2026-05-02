2e1f98eff68ca3dd91d57e4a801357780f7d6d15
Dmitriy <ChernakovDMT@yandex.ru>
2026-05-02 23:55:43 +0700

    Настройки автотестов с Pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,5 +1,5 @@
 from typing import Any, Generator
-
+from config import settings
 import pytest
 from _pytest.fixtures import SubRequest
 from playwright.sync_api import Page, Playwright
@@ -23,10 +23,14 @@ def initialize_browser_state(playwright: Playwright):
 
     registration_page = RegistrationPage(page=page)
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.registration_form.fill(
+        email=settings.test_user.email,
+        username=settings.test_user.username,
+        password=settings.test_user.password
+    )
     registration_page.click_registration_button()
 
-    context.storage_state(path="browser-state.json")
+    context.storage_state(path=settings.browser_state_file)
     browser.close()
 
 
@@ -40,5 +44,5 @@ def chromium_page_with_state(
     yield from initialize_playwright_page(
         playwright=playwright,
         test_name=request.node.name,
-        storage_state="browser-state.json"
+        storage_state=settings.browser_state_file
     )
